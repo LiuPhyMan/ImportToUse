0fcf6e72d960d4ec48e50689577ec64fcb31cf23
LiuPhyMan <liu.jinbao@outlook.com>
2018-10-03 09:59:10 +0800

    add __init__

---
+++ BetterQWidgets/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 14:12 2018/9/30
+
+@author:    Liu Jinbao
+@mail:      liu.jinbao@outlook.com
+@project:   ImportToUse
+@IDE:       PyCharm
+"""
+from __future__ import division, print_function, absolute_import
+from ._widgets import BetterButton, BetterQLabel, BetterQDoubleSpinBox, QPlot
+
+from numpy.testing import Tester
+
+test = Tester().test
