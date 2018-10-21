65380f376f5a4a336a5c87fdf3410b398f0db024
LiuPhyMan <liu.jinbao@outlook.com>
2018-10-21 09:03:01 +0800

    More readable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='ImportToUse',
+        version='1.0',
+        packages=['BetterQWidgets'],
+        url='',
+        license='',
+        author='PhyMan',
+        author_email='',
+        description=''
+        )
