0f10f65c591c79f4e81a79ebbe53038fd942275e
LiuJinbao <liu.jinbao@outlook.com>
2019-03-14 22:21:52 +0800

    Add BasicFunc

---
+++ BasicFunc/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on  15:56 2019/3/14
+
+@author:    Liu Jinbao
+@mail:      liu.jinbao@outlook.com
+@project:   ImportToUse
+@IDE:       PyCharm
+"""
+
+
+class tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        print("called : {n:.0f} numbers".format(n=self.calls))
+        return self.func(*args, **kwargs)
